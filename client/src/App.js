1acec457325fe8250c22ae236a11bbf2c446e7fb
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2020-09-16 15:58:22 +0900

    create wrapper box

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LabelBar from "./Component/LabelBar";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <LabelBar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/Component/LabelBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LabelBar = (props) => {
+  return (
+    <div className="total-list-box">
+      <label className="total-list">총 건의 게시물이 조회되었습니다.</label>
+    </div>
+  );
+};
+
+export default LabelBar;
