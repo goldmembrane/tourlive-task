4a36a54f691bafbe1a396476e86e0b88619d6435
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2020-09-17 10:57:05 +0900

    create list and mockup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import LabelBar from "./Component/LabelBar";
+import List from "./Component/List";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <LabelBar />
+        <List />
       </div>
     );
   }
