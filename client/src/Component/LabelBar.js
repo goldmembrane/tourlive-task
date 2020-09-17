9c7481c099fd64d83f9cd0c6109cea7d42e57657
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2020-09-17 13:43:38 +0900

    get tour list data from server

--- client/src/Component/LabelBar.js
+++ client/src/Component/LabelBar.js
@@ -4,7 +4,9 @@ import SearchBar from "./SearchBar";
 const LabelBar = (props) => {
   return (
     <div className="total-list-box">
-      <label className="total-list">총 건의 게시물이 조회되었습니다.</label>
+      <label className="total-list">
+        총 {props.total.length}건의 게시물이 조회되었습니다.
+      </label>
       <div className="search-bar-box">
         <SearchBar />
       </div>
