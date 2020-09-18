d87814e6639f163b35e20e1efa8082911085fbcb
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2020-09-18 10:42:41 +0900

    implement pagenation function

--- client/src/Component/PageNumber.js
+++ client/src/Component/PageNumber.js
@@ -1,7 +1,27 @@
 import React from "react";
+import _ from "lodash";
 
 const PageNumber = (props) => {
-  return <div></div>;
+  const { list, pageSize, change } = props;
+  const pageCount = Math.ceil(list.length / pageSize);
+
+  if (pageCount === 1) {
+    return null;
+  }
+
+  const pages = _.range(1, pageCount + 1);
+
+  return (
+    <nav>
+      <ul>
+        {pages.map((page) => (
+          <li key={page} style={{ cursor: "pointer" }}>
+            <a onClick={() => change(page)}>{page}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default PageNumber;
